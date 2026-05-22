08d6d43968f7af175c987ee72bd7bbc2bc56276c
Cecep Nandang <cecep@kima.one>
2026-05-22 17:26:06 +0700

    fix: bug

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000',
-  baseURL: import.meta.env.VITE_API_URL || 'https://api.kingcreativestudio.my.id.com/dapur-mbg',
+  baseURL: import.meta.env.VITE_API_URL || 'https://api.kingcreativestudio.my.id/dapur-mbg',
 })
 
 api.interceptors.request.use((config) => {
